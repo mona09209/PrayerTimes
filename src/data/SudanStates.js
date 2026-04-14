15dcc0fa2145617c9b331a5b028ba3b71cabe97e
Mona Mohamed <71096287+mona09209@users.noreply.github.com>
2026-04-14 13:07:26 +0200

    Update SudanStates.js

--- src/data/SudanStates.js
+++ src/data/SudanStates.js
@@ -1,22 +1,20 @@
-export const sudanCities = [
+const sudanCities = [
   { nameAr: "الخرطوم", city: "Khartoum", country: "SD" },
   { nameAr: "أم درمان", city: "Omdurman", country: "SD" },
-  { nameAr: "بحري", city: "Khartoum North", country: "SD" },
-  { nameAr: "ود مدني", city: "Wad Madani", country: "SD" },
+  { nameAr: "بحري", city: "Khartoum", country: "SD" },
+  { nameAr: "ود مدني", city: "Wad Medani", country: "SD" },
   { nameAr: "بورتسودان", city: "Port Sudan", country: "SD" },
   { nameAr: "كسلا", city: "Kassala", country: "SD" },
   { nameAr: "عطبرة", city: "Atbara", country: "SD" },
   { nameAr: "دنقلا", city: "Dongola", country: "SD" },
   { nameAr: "الأبيض", city: "El Obeid", country: "SD" },
   { nameAr: "كادوقلي", city: "Kadugli", country: "SD" },
-  { nameAr: "النهود", city: "An Nahud", country: "SD" },
   { nameAr: "كوستي", city: "Kosti", country: "SD" },
   { nameAr: "سنار", city: "Sennar", country: "SD" },
-  { nameAr: "الدمازين", city: "Ad Damazin", country: "SD" },
+  { nameAr: "الدمازين", city: "Damazin", country: "SD" },
   { nameAr: "القضارف", city: "Gedaref", country: "SD" },
-  { nameAr: "الفاشر", city: "El Fasher", country: "SD" },
+  { nameAr: "الفاشر", city: "Al Fashir", country: "SD" },
   { nameAr: "نيالا", city: "Nyala", country: "SD" },
-  { nameAr: "الجنينة", city: "Geneina", country: "SD" },
-  { nameAr: "زالنجي", city: "Zalingei", country: "SD" },
-  { nameAr: "الضعين", city: "Ed Daein", country: "SD" }
+  { nameAr: "الجنينة", city: "El Geneina", country: "SD" },
+  { nameAr: "زالنجي", city: "Zalingei", country: "SD" }
 ];
